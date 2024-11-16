07d7ec9771d4d35c3d1cd500d41220be5483ffcd
rein-77 <mmadan733@gmail.com>
2024-11-16 22:43:41 +0800

    small change

--- src/Koneksi.java
+++ src/Koneksi.java
@@ -1,12 +1,15 @@
+
 import java.sql.*;
 import java.io.File;
 
 public class Koneksi {
+
     // Variabel untuk menyimpan koneksi database
     private static Connection koneksi;
-    
+
     /**
      * Method untuk mendapatkan koneksi ke database SQLite
+     *
      * @return object Connection untuk koneksi ke database
      */
     public static Connection getKoneksi() {
@@ -14,13 +17,13 @@ public class Koneksi {
             try {
                 // Load driver SQLite
                 Class.forName("org.sqlite.JDBC");
-                
+
                 // Buat direktori database jika belum ada
                 File dbDir = new File("database");
                 if (!dbDir.exists()) {
                     dbDir.mkdir();
                 }
-                
+
                 // Buat koneksi ke database
                 String url = "jdbc:sqlite:database/db_kontak.db";
                 koneksi = DriverManager.getConnection(url);
